6a05524780b27643cecbff9340ac8db15580ea2b
Jason Raede <jason.raede@gmail.com>
2014-03-27 14:25:53 -0700

    Fixed index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 module.exports = {
   endpoint: require('./lib/endpoint'),
-  httperror: require('./lib/httperror')
+  httperror: require('./lib/httperror'),
+  request: require('./lib/request')
 };
